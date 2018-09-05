bf0e89df2f02cfbad4782808788bf77549a742d3
cactusboy <13159098@qq.com>
2018-09-05 16:10:56 +0800

    hello

--- order/hello.py
+++ order/hello.py
@@ -1,10 +1,24 @@
 from flask import Flask
+from imooc import route_imooc
 
 app = Flask(__name__)
+app.register_blueprint(route_imooc, url_prefix = "/imooc")
+
 
 @app.route('/')
 def hello_world():
     return 'hello world'
+
+
+@app.route("/api")
+def index():
+    return 'Index page'
+
+
+@app.route("/api/heool")
+def hello():
+    return 'hello'
+
 
 if __name__ == "__main__":
     app.run()
