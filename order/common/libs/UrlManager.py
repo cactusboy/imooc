6998ac93ad85d361ea5c83622303c8f793a3b913
cactusboy <13159098@qq.com>
2018-09-06 15:26:23 +0800

    sqlite3

---
+++ order/common/libs/UrlManager.py
@@ -0,0 +1,11 @@
+class UrlManager(object):
+    """定义一个类，"""
+
+    @staticmethod  # 静态方法
+    def buildUrl(path):
+        """定义连接地址"""
+        return path
+
+    @staticmethod
+    def buildStaticUrl(path):
+        return path
